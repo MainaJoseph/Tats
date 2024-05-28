f40a051dd11ef9fa3ae6414c20cc7fa0dd04130a
MainaJoseph <mainajm254@gmail.com>
2024-05-28 18:31:48 +0300

    Update login card

--- app/components/auth/CardWrapper.tsx
+++ app/components/auth/CardWrapper.tsx
@@ -1,5 +1,12 @@
 "use client";
-
+import React, { useRef } from "react";
+import {
+  motion,
+  useAnimationFrame,
+  useMotionTemplate,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
 import {
   Card,
   CardContent,
@@ -15,6 +22,9 @@ interface CardWrapperProps {
   backButtonLabel: string;
   backButtonHref: string;
   showSocial?: boolean;
+  duration?: number;
+  rx?: string;
+  ry?: string;
 }
 
 export const CardWrapper = ({
@@ -23,9 +33,34 @@ export const CardWrapper = ({
   backButtonHref,
   backButtonLabel,
   showSocial,
+  duration = 5000, // Increased duration for slower movement
+  rx = "0%",
+  ry = "0%",
 }: CardWrapperProps) => {
+  const pathRef = useRef<SVGRectElement>(null);
+  const progress = useMotionValue<number>(0);
+
+  useAnimationFrame((time) => {
+    const length = pathRef.current?.getTotalLength();
+    if (length) {
+      const pxPerMillisecond = length / duration;
+      progress.set((time * pxPerMillisecond) % length);
+    }
+  });
+
+  const x = useTransform(
+    progress,
+    (val) => pathRef.current?.getPointAtLength(val).x
+  );
+  const y = useTransform(
+    progress,
+    (val) => pathRef.current?.getPointAtLength(val).y
+  );
+
+  const transform = useMotionTemplate`translateX(${x}px) translateY(${y}px) translateX(-50%) translateY(-50%)`;
+
   return (
-    <Card className=" w-full shadow-md border border-sky-300 rounded-md">
+    <Card className="w-full shadow-md border border-sky-300 rounded-md relative">
       <CardHeader>
         <Header label={headerLabel} />
       </CardHeader>
@@ -35,6 +70,39 @@ export const CardWrapper = ({
           <Socials />
         </CardFooter>
       )}
+      <CardFooter>
+        <a href={backButtonHref} className="btn">
+          {backButtonLabel}
+        </a>
+      </CardFooter>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+        className="absolute h-full w-full"
+        width="100%"
+        height="100%"
+      >
+        <rect
+          fill="none"
+          stroke="transparent"
+          width="100%"
+          height="100%"
+          rx={rx}
+          ry={ry}
+          ref={pathRef}
+        />
+      </svg>
+      <motion.div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          display: "inline-block",
+          transform,
+        }}
+      >
+        <div className="h-2 w-2 bg-sky-500" />
+      </motion.div>
     </Card>
   );
 };
