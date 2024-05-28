e301e22ca24125c1d62681d11b924c44405fd24e
MainaJoseph <mainajm254@gmail.com>
2024-05-28 20:16:23 +0300

    Added Form

--- app/components/auth/LoginForm.tsx
+++ app/components/auth/LoginForm.tsx
@@ -1,6 +1,29 @@
+"use client";
+import { z } from "zod";
 import { CardWrapper } from "./CardWrapper";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas/index";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <CardWrapper
       headerLabel="Welcome Back"
@@ -8,7 +31,19 @@ export const LoginForm = () => {
       backButtonHref="/auth/sign-up"
       showSocial
     >
-      Login!
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
