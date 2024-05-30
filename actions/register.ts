3501e4e06d6f9a83225aa96f3b6728ca1c286456
MainaJoseph <mainajm254@gmail.com>
2024-05-30 17:03:24 +0300

    Created getUserByEmail data

--- actions/register.ts
+++ actions/register.ts
@@ -1,43 +1,49 @@
 "use server";
 
 import * as z from "zod";
-import bycrypt from "bcrypt";
+import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
-
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  // Validate on the server side
-  const validatedFields = RegisterSchema.safeParse(values);
+  try {
+    // Validate on the server side
+    const validatedFields = RegisterSchema.safeParse(values);
 
-  // Validating the data from Register schema
-  if (!validatedFields.success) {
-    return Promise.resolve({ error: "invalid Fields!" });
+    // Validating the data from Register schema
+    if (!validatedFields.success) {
+      return { error: "Invalid fields!" };
+    }
+
+    const { email, password, name } = validatedFields.data;
+
+    // Hashing the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Checking if the email exists
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return { error: "Email already in use!" };
+    }
+
+    // Create user
+    await db.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    // TO DO: Send verification Token Email
+
+    return { success: "User created" };
+  } catch (error) {
+    console.error("Error during user registration:", error);
+    return {
+      error: "An error occurred during registration. Please try again later.",
+    };
   }
-
-  const { email, password, name } = validatedFields.data;
-
-  // Hashing the password
-  const hashedPassword = await bycrypt.hash(password, 10);
-
-  //Checking if the email exists
-  const existingUser = await getUserByEmail(email);
-
-  if (existingUser) {
-    return { error: "Email already in use!" };
-  }
-
-  //Create user
-  await db.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  //TO DO: Send verification Token Email
-
-  return { success: "User Created" };
 };
