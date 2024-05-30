01fedaf5b67603e7c1af9641d113625d2e710967
MainaJoseph <mainajm254@gmail.com>
2024-05-30 23:39:34 +0300

    Updated login.ts

--- actions/login.ts
+++ actions/login.ts
@@ -2,6 +2,9 @@
 
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   // Validate on the server side
@@ -11,5 +14,27 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return Promise.resolve({ error: "invalid Fields!" });
   }
 
-  return Promise.resolve({ success: "Email sent" });
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return {
+            error: "Invalid Credentials",
+          };
+        default: {
+          return { error: "Something Went wrong!" };
+        }
+      }
+    }
+
+    throw error;
+  }
 };
