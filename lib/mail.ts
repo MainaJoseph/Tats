6f049a8baaa53b9eb29c89e04201139aa2871185
MainaJoseph <mainajm254@gmail.com>
2024-06-19 01:42:25 +0300

    Fixed email lib

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,10 +4,12 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const domain = process.env.NEXT_PUBLIC_APP_URL;
+
 // function to send verification Email from login and register page
 export const sendVerificationEmail = async (email: string, token: string) => {
   //Change in production
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   //send the email
   await resend.emails.send({
@@ -23,7 +25,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 // function to send password reset Email from reset password Page
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   //Change in production
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   //send the email
   await resend.emails.send({
