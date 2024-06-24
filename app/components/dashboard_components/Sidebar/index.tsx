7fb9c2a70fcbb8f242487d92b14723524cb73df4
MainaJoseph <mainajm254@gmail.com>
2024-06-25 02:28:34 +0300

    Added account in the sidebar

--- app/components/dashboard_components/Sidebar/index.tsx
+++ app/components/dashboard_components/Sidebar/index.tsx
@@ -13,6 +13,7 @@ import { Redressed } from "next/font/google";
 
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { useCurrentUser } from "@/hooks/use-current-user";
+import { IoMenuSharp } from "react-icons/io5";
 
 interface SidebarProps {
   sidebarOpen: boolean;
@@ -294,7 +295,11 @@ const menuGroups = [
   },
 ];
 
-const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
+const Sidebar: React.FC<SidebarProps> = ({ sidebarOpen, setSidebarOpen }) => {
+  const handleItemClick = () => {
+    // Close sidebar when a menu item is clicked
+    setSidebarOpen(false);
+  };
   const pathname = usePathname();
   const [pageName, setPageName] = useLocalStorage("selectedMenu", "dashboard");
   const user = useCurrentUser();
@@ -318,10 +323,10 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               href="/dashboard"
               className={`${redressed.className} font-bold text-3xl flex flex-row`}
             >
-              Tats
               <span className="mt-0">
                 <Image src="/tatswhite.png" alt="Tats" width={50} height={50} />
               </span>
+              Tats
             </Link>
           </div>
 
@@ -330,19 +335,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
             aria-controls="sidebar"
             className="block lg:hidden"
           >
-            <svg
-              className="fill-current"
-              width="20"
-              height="18"
-              viewBox="0 0 20 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
-                fill=""
-              />
-            </svg>
+            <IoMenuSharp size={25} />
           </button>
         </div>
         {/* <!-- SIDEBAR HEADER --> */}
@@ -363,6 +356,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                       item={menuItem}
                       pageName={pageName}
                       setPageName={setPageName}
+                      onClick={handleItemClick} // Pass onClick handler to SidebarItem
                     />
                   ))}
                 </ul>
