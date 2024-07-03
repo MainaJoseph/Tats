5fc222aab4a0d801169759f18c86ceec33aadb6b
MainaJoseph <mainajm254@gmail.com>
2024-07-03 18:44:43 +0300

    Added transaction calender

--- app/(protected)/transactions/transactions-client.tsx
+++ app/(protected)/transactions/transactions-client.tsx
@@ -4,70 +4,164 @@ import * as React from "react";
 import { useState, useCallback, useEffect } from "react";
 import axios from "axios";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { toast } from "react-toastify";
 import Spinner from "@/app/components/Spinner";
 import Heading from "@/app/components/Heading";
-import { Calendar } from "@/components/ui/calendar";
+import { DateRangePicker, RangeKeyDict } from "react-date-range";
 import { IconButton, Modal, Box } from "@mui/material";
 import { MdCalendarToday } from "react-icons/md";
+import { useToast } from "@/components/ui/use-toast"; // Import the useToast hook
+import "react-date-range/dist/styles.css"; // Main style file for date range picker
+import "react-date-range/dist/theme/default.css"; // Theme CSS file for date range picker
 
 interface Transaction {
-  isPosted: boolean;
+  id: string;
   transactionId: string;
+  createdAt: string;
+  fdcNumber: string;
   fdcName: string;
+  rdgIndex: string;
   rdgId: string;
+  fp: string;
   pumpAddress: string;
   nozzle: string;
-  fromDate: string;
+  fdcTransactionId: string;
+  fdcDateTime: string;
+  rdgTransactionId: string;
+  rdgDateTime: string;
+  productId: string;
+  productName: string;
+  price: number;
+  volume: number;
+  amount: number;
+  roundType: string;
+  totalVolume: number;
+  fdcTank: string;
 }
 
 const ManageTransactions: React.FC = () => {
+  const { toast } = useToast(); // Initialize the toast hook
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [selectedDate, setSelectedDate] = useState<Date | undefined>(
-    new Date()
-  );
+  const [dateRange, setDateRange] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
   const [isCalendarOpen, setIsCalendarOpen] = useState(false);
 
   const fetchTransactions = useCallback(async () => {
-    if (!selectedDate) return;
+    const { startDate, endDate } = dateRange;
+    if (!startDate || !endDate) return;
 
     setIsLoading(true);
     try {
-      const formattedDate = selectedDate.toISOString().split("T")[0];
-      console.log("Fetching transactions for date:", formattedDate);
+      const formattedStartDate = startDate.toISOString().split("T")[0];
+      const formattedEndDate = endDate.toISOString().split("T")[0];
 
       const response = await axios.get(
-        "http://20.4.219.173:8800/Transactions/fetchTransactions",
+        "http://20.4.219.173:8800/reports/v2/2?reportType=month",
         {
           params: {
-            fromDate: formattedDate,
+            fromDateTime: `${formattedStartDate}T00:00:00`,
+            toDateTime: `${formattedEndDate}T23:59:59`,
           },
         }
       );
-      console.log("API response:", response.data);
 
-      setTransactions(response.data);
+      if (response.data && response.data.report) {
+        // Extract and map transactions data with unique id
+        const mappedTransactions = response.data.report.map(
+          (item: any, index: number) => ({
+            id: `trans-${index}`,
+            transactionId: item.transactionId,
+            createdAt: item.datetime,
+            fdcNumber: item.fdcNumber,
+            fdcName: item.fdcName,
+            rdgIndex: item.rdgIndex,
+            rdgId: item.rdgId,
+            fp: item.fp,
+            pumpAddress: item.pumpAddress,
+            nozzle: item.nozzle,
+            fdcTransactionId: item.fdcTransactionId,
+            fdcDateTime: item.fdcDateTime,
+            rdgTransactionId: item.rdgTransactionId,
+            rdgDateTime: item.rdgDateTime,
+            productId: item.productId,
+            productName: item.productname,
+            price: item.price,
+            volume: item.volume,
+            amount: item.amount,
+            roundType: item.roundType,
+            totalVolume: item.totalVolume,
+            fdcTank: item.fdcTank,
+          })
+        );
+        setTransactions(mappedTransactions);
+      } else {
+        toast({
+          title: "No Transactions",
+          description: "No transactions found for the selected date range.",
+          style: {
+            backgroundColor: "#1e293b", // Slate-800
+            color: "#ffffff", // White
+          },
+        });
+      }
     } catch (error) {
-      console.error("Error fetching transactions:", error);
-      toast.error("Failed to fetch transactions.");
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 500) {
+          toast({
+            title: "Server Error",
+            description: "Internal server error. Please try again later.",
+            style: {
+              backgroundColor: "#1e293b", // Slate-800
+              color: "#ffffff", // White
+            },
+          });
+        } else {
+          toast({
+            title: "Fetch Error",
+            description:
+              "Failed to fetch transactions. Please try again later.",
+            style: {
+              backgroundColor: "#1e293b", // Slate-800
+              color: "#ffffff", // White
+            },
+          });
+        }
+      } else {
+        toast({
+          title: "Unknown Error",
+          description: "An unknown error occurred. Please try again later.",
+          style: {
+            backgroundColor: "#1e293b", // Slate-800
+            color: "#ffffff", // White
+          },
+        });
+      }
     } finally {
       setIsLoading(false);
     }
-  }, [selectedDate]);
+  }, [dateRange, toast]);
 
   useEffect(() => {
     fetchTransactions();
   }, [fetchTransactions]);
 
   const columns: GridColDef[] = [
-    { field: "isPosted", headerName: "Is Posted", width: 150 },
     { field: "transactionId", headerName: "Transaction ID", width: 150 },
-    { field: "fdcName", headerName: "FDC Name", width: 150 },
-    { field: "rdgId", headerName: "RDG ID", width: 150 },
     { field: "pumpAddress", headerName: "Pump Address", width: 150 },
-    { field: "nozzle", headerName: "Nozzle", width: 150 },
-    { field: "fromDate", headerName: "From Date", width: 150 },
+    { field: "fdcTransactionId", headerName: "FDC Transaction ID", width: 150 },
+    { field: "fdcDateTime", headerName: "FDC DateTime", width: 150 },
+    { field: "rdgTransactionId", headerName: "RDG Transaction ID", width: 150 },
+    { field: "rdgDateTime", headerName: "RDG DateTime", width: 150 },
+    { field: "productId", headerName: "Product ID", width: 150 },
+    { field: "productName", headerName: "Product Name", width: 150 },
+    { field: "price", headerName: "Price", width: 150 },
+    { field: "amount", headerName: "Amount", width: 150 },
+    { field: "createdAt", headerName: "Created At", width: 150 },
+    { field: "volume", headerName: "Volume", width: 150 },
+    { field: "count", headerName: "Count", width: 150 },
   ];
 
   return (
@@ -85,7 +179,7 @@ const ManageTransactions: React.FC = () => {
         <DataGrid
           rows={transactions}
           columns={columns}
-          getRowId={(row) => row.transactionId}
+          getRowId={(row) => row.id}
           initialState={{
             pagination: {
               paginationModel: { page: 0, pageSize: 9 },
@@ -115,14 +209,36 @@ const ManageTransactions: React.FC = () => {
             borderRadius: 1,
           }}
         >
-          <Calendar
-            mode="single"
-            selected={selectedDate}
-            onSelect={(date) => {
-              setSelectedDate(date);
-              setIsCalendarOpen(false);
+          <DateRangePicker
+            ranges={[dateRange]}
+            onChange={(ranges: RangeKeyDict) => {
+              const { selection } = ranges;
+              const { startDate, endDate } = selection;
+              if (
+                startDate &&
+                endDate &&
+                startDate <= endDate &&
+                endDate <= new Date()
+              ) {
+                setDateRange({
+                  startDate,
+                  endDate,
+                  key: "selection",
+                });
+                setIsCalendarOpen(false);
+              } else {
+                toast({
+                  title: "Invalid Date Range",
+                  description: "Invalid date range selected.",
+                  style: {
+                    backgroundColor: "#1e293b", // Slate-800
+                    color: "#ffffff", // White
+                  },
+                });
+              }
             }}
-            className="rounded-md border shadow"
+            months={1}
+            direction="vertical"
           />
         </Box>
       </Modal>
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 
 import NavBar from "./components/Nav/Nav";
+import { Toaster } from "@/components/ui/toaster";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -24,6 +25,7 @@ export default async function RootLayout({
       <html lang="en">
         <body className={`${poppins.className} text-slate-700`}>
           <main>{children}</main>
+          <Toaster />
         </body>
       </html>
     </SessionProvider>
