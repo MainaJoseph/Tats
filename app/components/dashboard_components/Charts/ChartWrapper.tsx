e0b7b58926bd8c64a1c359b061a4191569de5d82
MainaJoseph <mainajm254@gmail.com>
2024-07-05 16:43:49 +0300

    restrusctured code weekly chart

---
+++ app/components/dashboard_components/Charts/ChartWrapper.tsx
@@ -0,0 +1,30 @@
+// ChartWrapper.tsx
+"use client";
+
+import React, { useState } from "react";
+import ChartOne from "./ChartOne";
+import ChartTwo from "./ChartTwo";
+import ChartThree from "./ChartThree";
+import Breadcrumb from "../Breadcrumbs/Breadcrumb";
+
+const ChartWrapper: React.FC = () => {
+  const [sumCount, setSumCount] = useState<number>(0);
+
+  const handleSumCountChange = (newSumCount: number) => {
+    setSumCount(newSumCount);
+  };
+
+  return (
+    <>
+      <Breadcrumb pageName="Chart" />
+
+      <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
+        <ChartOne onSumCountChange={handleSumCountChange} />
+        <ChartTwo />
+        <ChartThree />
+      </div>
+    </>
+  );
+};
+
+export default ChartWrapper;
--- app/components/dashboard_components/Charts/page.tsx
+++ app/components/dashboard_components/Charts/page.tsx
@@ -1,26 +1,11 @@
-import ChartOne from "./ChartOne";
-import ChartTwo from "./ChartTwo";
-import ChartThree from "./ChartThree";
-import Breadcrumb from "../Breadcrumbs/Breadcrumb";
-import React, { useState } from "react";
+// Chart.tsx
+import ChartWrapper from "./ChartWrapper";
 
 const Chart: React.FC = () => {
-  const [sumCount, setSumCount] = useState<number>(0);
-
-  const handleSumCountChange = (newSumCount: number) => {
-    setSumCount(newSumCount);
-  };
-
   return (
-    <>
-      <Breadcrumb pageName="Chart" />
-
-      <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
-        <ChartOne onSumCountChange={handleSumCountChange} />
-        <ChartTwo />
-        <ChartThree />
-      </div>
-    </>
+    <div>
+      <ChartWrapper />
+    </div>
   );
 };
 
