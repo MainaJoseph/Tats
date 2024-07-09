b7fa398110d1f0805918bb9c10724ac9c4b80630
MainaJoseph <mainajm254@gmail.com>
2024-07-09 15:18:51 +0300

    Added Command Dialogue

--- app/components/dashboard_components/Header/command-dialogue.tsx
+++ app/components/dashboard_components/Header/command-dialogue.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useRouter } from "next/navigation";
 import {
   CalendarIcon,
   EnvelopeClosedIcon,
-  FaceIcon,
   GearIcon,
   PersonIcon,
-  RocketIcon,
 } from "@radix-ui/react-icons";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { TbTransactionYuan } from "react-icons/tb";
+import { VisuallyHidden } from "@reach/visually-hidden";
 
 import {
   Command,
@@ -19,8 +21,6 @@ import {
   CommandShortcut,
   CommandDialog,
 } from "@/components/ui/command";
-import { MdOutlineDashboardCustomize } from "react-icons/md";
-import { TbTransactionYuan } from "react-icons/tb";
 
 interface CommandDialogueProps {
   open: boolean;
@@ -31,17 +31,35 @@ const CommandDialogue: React.FC<CommandDialogueProps> = ({
   open,
   onOpenChange,
 }) => {
+  const router = useRouter();
+
+  const handleDashboardClick = () => {
+    console.log("Dashboard clicked");
+    router.push("/dashboard");
+  };
+
+  const handleTransactionClick = () => {
+    console.log("Dashboard clicked");
+    router.push("/dashboard");
+  };
+
   return (
     <CommandDialog open={open} onOpenChange={onOpenChange}>
+      <div className="visually-hidden">
+        <h2 className="font-bold ml-4">Command Dialog</h2>
+      </div>
       <CommandInput placeholder="Type a command or search..." />
       <CommandList className="text-slate-800">
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup heading="Suggestions">
-          <CommandItem>
+          <CommandItem
+            className="cursor-pointer"
+            onSelect={handleDashboardClick}
+          >
             <MdOutlineDashboardCustomize className="mr-2 h-4 w-4" />
             <span className="text-slate-950">DashBoard</span>
           </CommandItem>
-          <CommandItem>
+          <CommandItem onSelect={handleTransactionClick}>
             <CalendarIcon className="mr-2 h-4 w-4" />
             <span>Transaction</span>
           </CommandItem>
