5d9d0ec5106e5b84c088df4d07690ee8993590b1
MainaJoseph <mainajm254@gmail.com>
2024-07-15 17:39:52 +0300

    Toast station if it exists

--- app/(protected)/add_stations/add-station.tsx
+++ app/(protected)/add_stations/add-station.tsx
@@ -1,19 +1,16 @@
-// components/AddStationForm.tsx
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { StationSchema, StationData } from "../../../schemas/index";
 import { useToast } from "@/components/ui/use-toast";
-import { useState } from "react";
 import { ScaleLoader } from "react-spinners";
 import Heading from "@/app/components/Heading";
 
 const AddStationForm: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
-
   const { toast } = useToast();
   const {
     register,
@@ -29,24 +26,50 @@ const AddStationForm: React.FC = () => {
     },
   });
 
+  const checkStationExists = async (name: string): Promise<boolean> => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL_STATION_ID}/stations`
+      );
+      const stations = response.data;
+      return stations.some(
+        (station: any) => station.name.toLowerCase() === name.toLowerCase()
+      );
+    } catch (error) {
+      console.error("Error checking station existence:", error);
+      return false;
+    }
+  };
+
   const onSubmit = async (data: StationData) => {
     setIsLoading(true);
     try {
-      const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL_STATION_ID}/stations`,
-        {
-          ...data,
-          id: 0,
-          pumps: {},
-          client: { id: 1 },
-        }
-      );
-      reset();
-      toast({
-        title: "Station Added Successfully",
-        description: `${data.name} has been added to the Tats.`,
-        className: "bg-slate-800 text-white rounded-md",
-      });
+      const stationExists = await checkStationExists(data.name);
+
+      if (stationExists) {
+        toast({
+          title: "Station Already Exists",
+          description: `A station with the name "${data.name}" already exists.`,
+          variant: "destructive",
+          className: "bg-slate-800 text-white rounded-md",
+        });
+      } else {
+        const response = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL_STATION_ID}/stations`,
+          {
+            ...data,
+            id: 0,
+            pumps: {},
+            client: { id: 1 },
+          }
+        );
+        reset();
+        toast({
+          title: "Station Added Successfully",
+          description: `${data.name} has been added to the Tats.`,
+          className: "bg-slate-800 text-white rounded-md",
+        });
+      }
     } catch (error) {
       console.error("Error adding station:", error);
       toast({
