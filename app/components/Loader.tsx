19ba87ceb311c72d623fd8fa78b6c16b461050f7
MainaJoseph <mainajm254@gmail.com>
2024-07-16 17:06:21 +0300

    Updated loader color

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -6,7 +6,7 @@ const Loader = () => {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center">
-        <CirclesWithBar color="black" />
+        <CirclesWithBar color="#2f405d" />
       </div>
     </div>
   );
