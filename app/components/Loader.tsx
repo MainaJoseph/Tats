ef33058a6df08b148c423086a52d04dacae581bd
MainaJoseph <mainajm254@gmail.com>
2024-08-01 17:34:42 +0300

    Made loader look good

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -4,9 +4,16 @@ import { CirclesWithBar } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-screen items-center justify-center bg-gray-50">
       <div className="flex flex-col items-center">
-        <CirclesWithBar color="#2f405d" />
+        <CirclesWithBar
+          color="#001550"
+          height={100}
+          width={100}
+          ariaLabel="loading"
+          wrapperClass="mb-4"
+        />
+        <p className="text-md font-bold text-gray-600">TATS is loading...</p>
       </div>
     </div>
   );
