476ddb5c15d47b9e5e8f7e14b370dbd109758ce5
MainaJoseph <mainajm254@gmail.com>
2024-08-14 14:55:27 +0300

    Added edit stations

--- app/(protected)/stations/EditStationModal.tsx
+++ app/(protected)/stations/EditStationModal.tsx
@@ -59,11 +59,13 @@ const EditStationModal: React.FC<EditStationModalProps> = ({
   return (
     <form onSubmit={handleSubmit}>
       <DialogHeader>
-        <DialogTitle>Edit Station</DialogTitle>
+        <DialogTitle className="text-slate-800 font-semibold">
+          Edit Station
+        </DialogTitle>
       </DialogHeader>
       <div className="grid gap-4 py-4">
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-right">
+          <Label htmlFor="name" className="text-right font-semibold">
             Name
           </Label>
           <Input
@@ -71,10 +73,11 @@ const EditStationModal: React.FC<EditStationModalProps> = ({
             value={name}
             onChange={(e) => setName(e.target.value)}
             className="col-span-3"
+            style={{ borderRadius: "6px" }}
           />
         </div>
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="location" className="text-right">
+          <Label htmlFor="location" className="text-right font-semibold">
             Location
           </Label>
           <Input
@@ -82,25 +85,52 @@ const EditStationModal: React.FC<EditStationModalProps> = ({
             value={location}
             onChange={(e) => setLocation(e.target.value)}
             className="col-span-3"
+            style={{ borderRadius: "6px" }}
           />
         </div>
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="nozzleIdentifierName" className="text-right">
+          <Label
+            htmlFor="nozzleIdentifierName"
+            className="text-right font-semibold"
+          >
             Nozzle Identifier
           </Label>
-          <Input
+          <select
             id="nozzleIdentifierName"
             value={nozzleIdentifierName}
-            onChange={(e) => setNozzleIdentifierName(e.target.value)}
+            onChange={(e) =>
+              setNozzleIdentifierName(
+                e.target.value as "pumpAddress" | "nozzle"
+              )
+            }
             className="col-span-3"
-          />
+          >
+            <option value="pumpAddress" className=" bg-slate-800 text-white">
+              Pump Address
+            </option>
+            <option value="nozzle" className=" bg-slate-800 text-white">
+              Nozzle
+            </option>
+          </select>
         </div>
       </div>
       <DialogFooter>
-        <Button type="button" variant="secondary" onClick={onClose}>
+        <Button
+          type="button"
+          variant="secondary"
+          onClick={onClose}
+          className="border border-slate-500 hover:border-rose-600"
+          style={{ borderRadius: "6px" }}
+        >
           Cancel
         </Button>
-        <Button type="submit">Save Changes</Button>
+        <Button
+          type="submit"
+          className="bg-sky-500 text-white hover:bg-sky-500/70"
+          style={{ borderRadius: "6px" }}
+        >
+          Save Changes
+        </Button>
       </DialogFooter>
     </form>
   );
